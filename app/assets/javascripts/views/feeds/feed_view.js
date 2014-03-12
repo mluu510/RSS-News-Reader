460f54c1fd84e528068552392ef7d5b59fca3697
App Academy Student <student@appacademy.io>
2014-03-12 16:40:31 -0700

    done

--- app/assets/javascripts/views/feeds/feed_view.js
+++ app/assets/javascripts/views/feeds/feed_view.js
@@ -2,7 +2,6 @@ NewReader.Views.FeedView = Backbone.View.extend({
 	template: JST['feeds/show'],
 
 	render: function() {
-		console.log('rendering');
 		var content = this.template({ feed: this.model });
 		this.$el.html(content);
 		return this;
@@ -17,7 +16,6 @@ NewReader.Views.FeedView = Backbone.View.extend({
 	},
 
 	handleRefresh: function(event) {
-		console.log('refreshing');
 		this.model.fetch();
 	}
 })
